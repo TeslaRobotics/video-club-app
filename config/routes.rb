67158506bb57656a595da44a55a150ca5a2169d2
Ricardo <ry.yrupailla@gmail.com>
2019-04-01 17:43:07 -0500

    Add welcome route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
+  get 'welcome', to: 'welcome#index'
   resources :orders
   resources :castings
   resources :cassettes
